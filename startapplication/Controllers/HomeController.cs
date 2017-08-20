4a97aaf386e1e3f7958ff7429994985c475bfb53
SAPIENT\rrusta <rustagirohit@gmail.com>
2017-08-20 19:04:14 +0530

    no message

---
+++ startapplication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using startapplication.Models;
+
+namespace startapplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            IISExpress objIISExpress = new IISExpress(@"C:\Users\rrusta\Documents\IISExpress\config\applicationhost.config",
+            @"mysnippets",
+            @"Clr4IntegratedAppPool");
+            return View();
+        }
+	}
+}
